0cf40247cdf7a024129807587cd78e9619fd881f
Blake Spencer <blake.spencer1@gmail.com>
2018-10-09 15:36:55 -0400

    [update] models changed to have user: password and products: price, db sync in server file has force: true

--- db/models/index.js
+++ db/models/index.js
@@ -1,9 +1,9 @@
-const User = require("./users");
-const Product = require("./products");
-const Order = require("./orders");
+const User = require('./users');
+const Product = require('./products');
+const Order = require('./orders');
 
-Order.Products = Order.belongsToMany(Product, { through: "Item" });
-Product.Orders = Product.belongsToMany(Order, { through: "Item" });
+Order.Products = Order.belongsToMany(Product, { through: 'Item' });
+Product.Orders = Product.belongsToMany(Order, { through: 'Item' });
 
 User.Orders = User.hasMany(Order);
 
--- server.js
+++ server.js
@@ -1,20 +1,30 @@
-const express = require("express");
-const expressGraphQL = require("express-graphql");
-const schema = require("./schema.js");
-const cors = require("cors");
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema.js');
+const cors = require('cors');
+const db = require('./db');
 
 const app = express();
 
 app.use(cors());
 //all requests go through here
 app.use(
-  "/graphql",
+  '/graphql',
   expressGraphQL({
     schema,
-    graphiql: true
+    graphiql: true,
   })
 );
 
-app.listen(4000, () => {
-  console.log("server running on port 4000");
-});
+const dbSyncListen = async () => {
+  try {
+    await db.sync({ force: true });
+    app.listen(4000, () => {
+      console.log('server running on port 4000');
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+dbSyncListen();
