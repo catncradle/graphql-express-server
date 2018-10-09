acd5f751b2315fbb42f6dc0eadda091662d6a50c
Jack Lye <catncradle@gmail.com>
2018-10-09 14:53:47 -0400

    added cors on backend

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-const express = require('express');
-const expressGraphQL = require('express-graphql');
-const schema = require('./schema.js');
+const express = require("express");
+const expressGraphQL = require("express-graphql");
+const schema = require("./schema.js");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 //all requests go through here
 app.use(
-  '/graphql',
+  "/graphql",
   expressGraphQL({
     schema,
-    graphiql: true,
+    graphiql: true
   })
 );
 
 app.listen(4000, () => {
-  console.log('server running on port 4000');
+  console.log("server running on port 4000");
 });
